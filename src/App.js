860aa91227ecde6a5b88002a644dcb638b27e70f
neudopb <neudinho66@gmail.com>
2021-12-12 23:32:16 -0300

    Estilização Material-UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { Component, Fragment } from 'react';
-import './App.css';
-import FormCadastro from './components/FormCadastro/FormCadastro';
+import React, { Component } from "react";
+import "./App.css";
+import FormCadastro from "./components/FormCadastro/FormCadastro";
+import { Container, Typography } from "@material-ui/core";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <h1>Formulario de Cadastro</h1>
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center">
+          Formulario de Cadastro
+        </Typography>
         <FormCadastro />
-      </Fragment>
+      </Container>
     );
   }
 }
--- src/components/FormCadastro/FormCadastro.jsx
+++ src/components/FormCadastro/FormCadastro.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormCadastro() {
-    return (
-        <form>
-            <TextField id="nome" label="Nome" variant="outlined" fullWidth />
-            <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth />
-            <TextField id="cpf" label="CPF" variant="outlined" fullWidth />
+  return (
+    <form>
+      <TextField
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
 
-            <label>Promoções</label>
-            <input type="checkbox" />
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promocoes" defaultCheck color="primary" />}
+      />
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultCheck color="primary" />}
+      />
 
-            <label>Novidades</label>
-            <input type="checkbox" />
-
-            <Button 
-                type='submit' 
-                variant='contained' 
-                color='primary'
-            >
-                Cadastrar
-            </Button>
-
-        </form>
-    );
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
 }
 
 export default FormCadastro;
