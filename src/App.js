c9c22abbe07b3181e665da52c6677fe4f7e73c5a
neudopb <neudinho66@gmail.com>
2021-12-12 22:49:37 -0300

    Inicio Form com Material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import FormCadastro from './components/FormCadastro/FormCadastro';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Formulario de Cadastro</h1>
+        <FormCadastro />
+      </Fragment>
+    );
+  }
 }
 
 export default App;
+
+// npx create-react-app nome-projeto
+// npm start
+// npm audit fix --force
