b311c40b1a824a32bf700ad9bc336921f298da33
neudopb <neudinho66@gmail.com>
2021-12-14 10:56:46 -0300

    Criação inicial dos 3 forms

---
+++ src/components/FormCadastro/DadosUsuario.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button, TextField } from "@material-ui/core";
+
+function DadosUsuario() {
+  return (
+    <form>
+      <TextField
+        id="email"
+        label="Email"
+        type="email"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <TextField
+        id="senha"
+        label="Senha"
+        type="password"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <Button type="submit" variant="contained" color="primary">
+        Proximo
+      </Button>
+    </form>
+  );
+}
+
+export default DadosUsuario;
--- src/components/FormCadastro/FormCadastro.jsx
+++ src/components/FormCadastro/FormCadastro.jsx
@@ -1,99 +1,16 @@
-import React, { useState } from "react";
-import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
+import React from "react";
+import DadosEntrega from "./DadosEntrega";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
 
 function FormCadastro({enviarDados, validarCpf}) {
 
-  const [nome, setNome] = useState("");
-  const [sobrenome, setSobrenome] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [promocoes, setPromocoes] = useState(true);
-  const [novidades, setNovidades] = useState(true);
-  const [erros, setErros] = useState({cpf:{valido:true, texto:""}});
-
   return (
-    <form
-      onSubmit={(evento) => {
-        evento.preventDefault();
-        enviarDados({ nome, sobrenome, cpf, promocoes, novidades });
-      }}
-    >
-
-      <TextField
-        value={nome}
-        onChange={(evento) => {
-          setNome(evento.target.value);
-        }}
-        id="nome"
-        label="Nome"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-      />
-      
-      <TextField
-        value={sobrenome}
-        onChange={(evento) => {
-          setSobrenome(evento.target.value);
-        }}
-        id="sobrenome"
-        label="Sobrenome"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-      />
-      
-      <TextField
-        value={cpf}
-        onChange={(evento) => {
-          if (evento.target.value.length <= 11) {
-            setCpf(evento.target.value);
-          }
-        }}
-        onBlur={(event) => {
-          const valid = validarCpf(cpf);
-          setErros({cpf:valid});
-        }}
-        error={!erros.cpf.valido}
-        helperText={erros.cpf.texto}
-        id="cpf"
-        label="CPF"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-      />
-
-      <FormControlLabel
-        label="Promoções"
-        control={
-          <Switch
-            checked={promocoes}
-            onChange={(evento) => {
-              setPromocoes(evento.target.checked);
-            }}
-            name="promocoes"
-            color="primary"
-          />
-        }
-      />
-      
-      <FormControlLabel
-        label="Novidades"
-        control={
-          <Switch
-            checked={novidades}
-            onChange={(evento) => {
-              setNovidades(evento.target.checked);
-            }}
-            name="novidades"
-            color="primary"
-          />
-        }
-      />
-
-      <Button type="submit" variant="contained" color="primary">
-        Cadastrar
-      </Button>
-    </form>
+    <>
+      <DadosPessoais enviarDados={enviarDados} validarCpf={validarCpf} />
+      <DadosUsuario />
+      <DadosEntrega />
+    </>
   );
 }
 
